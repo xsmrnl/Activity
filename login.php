b7a207c4056f84010120429b4d74e03dbea7707d
xsmrnl <131590581+xsmrnl@users.noreply.github.com>
2024-12-06 02:45:44 +0800

    Sample (#5)
    * Ganito ata 'yon siya, check mo.
    
    * Final code.
    
    ---------
    
    Co-authored-by: marinelaaa <buenaventurasm10@gmail.com>

--- login.php
+++ login.php
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-<? php
-$user = "Sarah";
-$pass = "1234";
-
-
-switch ($user) {
-    case "Sarah":
-        echo "You are logged in!<br>";
-        echo "Welcome, Sarah!<br>";
-        break;
-
-    case "Llano":
-        echo "You are logged in!<br>";
-        echo "Welcome, Llano!<br>";
-        break;
-
-    default:
-        echo "Account not found!<br>";
-        break;
-}
-
-switch ($pass) {
-    case "1234":
-        echo "Password is correct!<br>";
-        break;
-
-    case "5678":
-        echo "Password is correct but belongs to another user!<br>";
-        break;
-
-    default:
-        echo "Incorrect password!<br>";
-        break;
-}
-
-
-
-
-?>
-
-
-
-=======
-
-
-
 <?php
 
 $user = "Sarah";
@@ -84,6 +37,6 @@ switch ($user) {
 
 ?>
 
->>>>>>> main
+
 
 
