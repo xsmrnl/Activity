1917e5626db93c6fc23dbcd5de21e657aa6882e8
marinelaaa <buenaventurasm10@gmail.com>
2024-12-06 02:40:43 +0800

    Final code.

--- login.php
+++ login.php
@@ -1,49 +1,6 @@
 <<<<<<< HEAD
-<? php
-$user = "Sarah";
-$pass = "1234";
-
-
-switch ($user) {
-    case "Sarah":
-        echo "You are logged in!<br>";
-        echo "Welcome, Sarah!<br>";
-        break;
-
-    case "Llano":
-        echo "You are logged in!<br>";
-        echo "Welcome, Llano!<br>";
-        break;
-
-    default:
-        echo "Account not found!<br>";
-        break;
-}
-
-switch ($pass) {
-    case "1234":
-        echo "Password is correct!<br>";
-        break;
-
-    case "5678":
-        echo "Password is correct but belongs to another user!<br>";
-        break;
-
-    default:
-        echo "Incorrect password!<br>";
-        break;
-}
-
-
-
-
-?>
-
-
 
 =======
-
-
 
 <?php
 
@@ -84,6 +41,5 @@ switch ($user) {
 
 ?>
 
->>>>>>> main
 
-
+>>>>>>> sample
