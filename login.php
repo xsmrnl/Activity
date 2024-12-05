18e1c88ab5d243ce771f8a46367d915043b17729
xsmrnl <131590581+xsmrnl@users.noreply.github.com>
2024-12-06 02:28:46 +0800

    Ganito ata 'yon siya, check mo. (#4)
    Co-authored-by: marinelaaa <buenaventurasm10@gmail.com>

--- login.php
+++ login.php
@@ -1,29 +1,6 @@
 <<<<<<< HEAD
 <? php
 $user = "Sarah";
-
-switch ($user) {
-    case "Sarah":
-        echo "You are logged in!";
-        echo "Welcome, Sarah!";
-        break;
-
-    case "Llano":
-        echo "You are logged in!";
-        echo "Welcome, Llano";
-        break;
-    
-    default:
-        echo "Account not found!!!!";
-        break;
-}
-
-
-
-=======
-
-
-$user = "Sarah";
 $pass = "1234";
 
 
@@ -58,6 +35,51 @@ switch ($pass) {
 }
 
 
+
+
+?>
+
+
+
+=======
+
+
+
+<?php
+
+$user = "Sarah";
+$pass = "1234";
+
+$passwords = [
+    "Sarah" => "1234",
+    "Llano" => "5678"
+];
+
+switch ($user) {
+    case "Sarah":
+        if ($pass === $passwords["Sarah"]) {
+            echo "You are logged in!<br>";
+            echo "Welcome, Sarah!<br>";
+            echo "Password is correct!<br>";
+        } else {
+            echo "Incorrect password!<br>";
+        }
+        break;
+
+    case "Llano":
+        if ($pass === $passwords["Llano"]) {
+            echo "You are logged in!<br>";
+            echo "Welcome, Llano!<br>";
+            echo "Password is correct!<br>";
+        } else {
+            echo "Incorrect password!<br>";
+        }
+        break;
+
+    default:
+        echo "Account not found!<br>";
+        break;
+}
 
 
 ?>
