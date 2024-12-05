9171de046b841c80d07c1a60a3d31347786c2d20
marinelaaa <buenaventurasm10@gmail.com>
2024-12-06 01:42:05 +0800

    try this one

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <? php
 $user = "Sarah";
 
@@ -13,8 +14,54 @@ switch ($user) {
         break;
     
     default:
-        echo "Account not found!";
-        break; 
+        echo "Account not found!!!!";
+        break;
 }
+
+
+
+=======
+
+
+$user = "Sarah";
+$pass = "1234";
+
+
+switch ($user) {
+    case "Sarah":
+        echo "You are logged in!<br>";
+        echo "Welcome, Sarah!<br>";
+        break;
+
+    case "Llano":
+        echo "You are logged in!<br>";
+        echo "Welcome, Llano!<br>";
+        break;
+
+    default:
+        echo "Account not found!<br>";
+        break;
+}
+
+switch ($pass) {
+    case "1234":
+        echo "Password is correct!<br>";
+        break;
+
+    case "5678":
+        echo "Password is correct but belongs to another user!<br>";
+        break;
+
+    default:
+        echo "Incorrect password!<br>";
+        break;
+}
+
+
+
+
 ?>
+
+>>>>>>> main
+
 
